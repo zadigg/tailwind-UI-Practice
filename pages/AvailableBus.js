821e4954534ac546e4e9d11ca84f4e448a1d4032
zadigg <62830716+zadigg@users.noreply.github.com>
2022-04-03 23:31:07 +0300

    UI Update

--- pages/AvailableBus.js
+++ pages/AvailableBus.js
@@ -7,7 +7,7 @@ function AvailableBus() {
       <div className=" bg-gradient-to-r from-gray-200 via-white to-gray-200  bg-opacity-95 max-w-screen-xl mx-auto h-[100vh] rounded-2xl  ">
         <Header />
         <div className="bg-gradient-to-b from-[#F4F1E9] via-[#FCF0DB] to-gray-[#F4F2E3]  h-[50vh] md:max-w-3xl mx-auto space-y-8 mt-10 md:mt-16 lg:mt-20   ">
-          hello sasdsssasssassssdfs
+          hello sasdsssasssassssdfss
         </div>
       </div>
     </div>
